437eebf4585f35d68a9aa97408e38ec82481a1e4
Anders Østby <anders1989@gmail.com>
2020-02-03 20:42:01 +0100

    Fjerner logging fra health check

--- src/main/kotlin/com/andersostby/webhook/HealthCheck.kt
+++ src/main/kotlin/com/andersostby/webhook/HealthCheck.kt
@@ -4,13 +4,9 @@ import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.routing.Route
 import io.ktor.routing.options
-import org.slf4j.LoggerFactory
-
-private val log = LoggerFactory.getLogger("HealthCheck")
 
 internal fun Route.healthCheck() {
     options("/isAlive") {
-        log.info("Health check performed")
         call.response.status(HttpStatusCode.OK)
     }
 }
