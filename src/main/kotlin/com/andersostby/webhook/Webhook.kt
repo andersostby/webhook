7b3d9186a1e748f0aef6cfcf84662b2af31cbdd1
Anders Østby <anders1989@gmail.com>
2020-02-03 15:44:47 +0100

    Flytter interceptorene inni /webhook-route

--- src/main/kotlin/com/andersostby/webhook/Webhook.kt
+++ src/main/kotlin/com/andersostby/webhook/Webhook.kt
@@ -10,6 +10,7 @@ import io.ktor.request.header
 import io.ktor.request.receiveText
 import io.ktor.routing.Route
 import io.ktor.routing.post
+import io.ktor.routing.route
 import io.ktor.util.AttributeKey
 import org.slf4j.LoggerFactory
 
@@ -25,51 +26,53 @@ internal class Webhook {
     }
 
     internal fun Route.webhook() {
-        val signatureKey = AttributeKey<String>("signature")
-        val bodyKey = AttributeKey<String>("body")
-        intercept(ApplicationCallPipeline.Setup) {
-            val signature = call.request.header("X-Hub-Signature") ?: run {
-                call.response.status(HttpStatusCode.BadRequest)
-                finish()
-                return@intercept
+        route("/webhook") {
+            val signatureKey = AttributeKey<String>("signature")
+            val bodyKey = AttributeKey<String>("body")
+            intercept(ApplicationCallPipeline.Setup) {
+                val signature = call.request.header("X-Hub-Signature") ?: run {
+                    call.response.status(HttpStatusCode.BadRequest)
+                    finish()
+                    return@intercept
+                }
+
+                call.attributes.put(signatureKey, signature)
+                call.attributes.put(bodyKey, call.receiveText())
             }
-
-            call.attributes.put(signatureKey, signature)
-            call.attributes.put(bodyKey, call.receiveText())
-        }
-        intercept(ApplicationCallPipeline.Features) {
-            try {
-                Hmac(
-                        secret = "Hemmelig",
-                        signature = call.attributes[signatureKey],
-                        payload = call.attributes[bodyKey]
-                ).verify()
-            } catch (e: IllegalArgumentException) {
-                call.response.status(HttpStatusCode.Unauthorized)
-                finish()
-                return@intercept
-            } catch (e: IllegalStateException) {
-                call.response.status(HttpStatusCode.Unauthorized)
-                finish()
-                return@intercept
+            intercept(ApplicationCallPipeline.Features) {
+                try {
+                    Hmac(
+                            secret = "Hemmelig",
+                            signature = call.attributes[signatureKey],
+                            payload = call.attributes[bodyKey]
+                    ).verify()
+                } catch (e: IllegalArgumentException) {
+                    call.response.status(HttpStatusCode.Unauthorized)
+                    finish()
+                    return@intercept
+                } catch (e: IllegalStateException) {
+                    call.response.status(HttpStatusCode.Unauthorized)
+                    finish()
+                    return@intercept
+                }
             }
-        }
-        post("/webhook") {
-            log.info("Mottatt varsel")
-            val hook = jacksonObjectMapper()
-                    .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                    .readTree(call.attributes[bodyKey])
+            post {
+                log.info("Mottatt varsel")
+                val hook = jacksonObjectMapper()
+                        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                        .readTree(call.attributes[bodyKey])
 
-            val dockerTag = hook.requiredAt("/package/registry/url").textValue()
-                    .replace("^https?://(.+)".toRegex()) { it.groupValues[1] } + "/" +
-                    hook.requiredAt("/package/name").textValue() + ":" +
-                    hook.requiredAt("/package/package_version/version").textValue()
+                val dockerTag = hook.requiredAt("/package/registry/url").textValue()
+                        .replace("^https?://(.+)".toRegex()) { it.groupValues[1] } + "/" +
+                        hook.requiredAt("/package/name").textValue() + ":" +
+                        hook.requiredAt("/package/package_version/version").textValue()
 
-            log.info("Ny versjon: $dockerTag")
+                log.info("Ny versjon: $dockerTag")
 
-            listeners.forEach { it(dockerTag) }
+                listeners.forEach { it(dockerTag) }
 
-            call.response.status(HttpStatusCode.OK)
+                call.response.status(HttpStatusCode.OK)
+            }
         }
     }
 }
