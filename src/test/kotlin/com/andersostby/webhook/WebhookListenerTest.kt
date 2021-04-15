e2d0312ad38d5181acedf79f2b87950499a973b1
Anders Østby <anders1989@gmail.com>
2021-04-15 22:28:12 +0200

    Tester håndtering av tags

--- src/test/kotlin/com/andersostby/webhook/WebhookListenerTest.kt
+++ src/test/kotlin/com/andersostby/webhook/WebhookListenerTest.kt
@@ -10,19 +10,21 @@ import kotlin.test.assertNull
 
 internal class WebhookListenerTest {
     private val body =
-        "{\"package\":{\"name\":\"house-gh-webhook\",\"package_version\":{\"version\":\"453b786\"},\"registry\":{\"url\":\"https://docker.pkg.github.com/andersostby/house-gh-webhook\"}}}"
+        """{"package":{"name":"house-gh-webhook","package_version":{"version":"453b786"},"registry":{"url":"https://docker.pkg.github.com/andersostby/house-gh-webhook"}}}"""
+    private val tagBody =
+        """{"ref": "deployer-525eda2","ref_type": "tag"}"""
 
     private var resultPayload: String? = null
     private var responseStatus: HttpStatusCode? = null
 
     @BeforeEach
-    internal fun before() {
+    fun before() {
         resultPayload = null
         responseStatus = null
     }
 
     @Test
-    internal fun `Listener blir kalt med nytt event når webhook trigges med gydlig signatur`() {
+    fun `Listener blir kalt med nytt event når webhook trigges med gydlig signatur`() {
         val webhook = Webhook("Hemmelig")
         withTestApplication({
             routing {
@@ -42,14 +44,14 @@ internal class WebhookListenerTest {
 
             assertEquals(HttpStatusCode.OK, responseStatus)
             assertEquals(
-                "{\"registry\":\"https://docker.pkg.github.com/andersostby/house-gh-webhook\",\"app\":\"house-gh-webhook\",\"version\":\"453b786\",\"tag\":\"docker.pkg.github.com/andersostby/house-gh-webhook/house-gh-webhook:453b786\"}",
+                """{"registry":"https://docker.pkg.github.com/andersostby/house-gh-webhook","app":"house-gh-webhook","version":"453b786","tag":"docker.pkg.github.com/andersostby/house-gh-webhook/house-gh-webhook:453b786"}""",
                 resultPayload
             )
         }
     }
 
     @Test
-    internal fun `Listener blir ikke kalt med nytt event når webhook trigges med ugydlig signatur - gir 401`() {
+    fun `Listener blir ikke kalt med nytt event når webhook trigges med ugydlig signatur - gir 401`() {
         val webhook = Webhook("Hemmelig")
         withTestApplication({
             routing {
@@ -73,7 +75,7 @@ internal class WebhookListenerTest {
     }
 
     @Test
-    internal fun `Listener blir ikke kalt med nytt event når webhook trigges med for kort signatur - gir 401`() {
+    fun `Listener blir ikke kalt med nytt event når webhook trigges med for kort signatur - gir 401`() {
         val webhook = Webhook("Hemmelig")
         withTestApplication({
             routing {
@@ -97,7 +99,7 @@ internal class WebhookListenerTest {
     }
 
     @Test
-    internal fun `Listener blir ikke kalt med nytt event når webhook trigges uten signatur - gir 400`() {
+    fun `Listener blir ikke kalt med nytt event når webhook trigges uten signatur - gir 400`() {
         val webhook = Webhook("Hemmelig")
         withTestApplication({
             routing {
@@ -120,7 +122,7 @@ internal class WebhookListenerTest {
     }
 
     @Test
-    internal fun `Returnerer Accepted når webhook trigges med gydlig signatur og ukjent X-GitHub-Event`() {
+    fun `Returnerer Accepted når webhook trigges med gydlig signatur og ukjent X-GitHub-Event`() {
         val webhook = Webhook("Hemmelig")
         withTestApplication({
             routing {
@@ -140,6 +142,33 @@ internal class WebhookListenerTest {
 
             assertEquals(HttpStatusCode.Accepted, responseStatus)
             assertNull(resultPayload)
+        }
+    }
+
+    @Test
+    fun `Listener blir kalt med nytt event når webhook trigges med tag og kjent app`() {
+        val webhook = Webhook("Hemmelig")
+        withTestApplication({
+            routing {
+                webhook.apply { webhook() }
+            }
+        }) {
+            webhook.addListener { resultPayload = it }
+            with(handleRequest {
+                method = HttpMethod.Post
+                uri = "/webhook"
+                addHeader("X-GitHub-Event", "create")
+                addHeader("X-Hub-Signature", "160180c665e438f9ef705de4abdf7ece2a8352fc")
+                setBody(tagBody)
+            }) {
+                responseStatus = response.status()
+            }
+
+            assertEquals(HttpStatusCode.OK, responseStatus)
+            assertEquals(
+                """{"app":"deployer","version":"525eda2","tag":"ghcr.io/andersostby/deployer:525eda2"}""",
+                resultPayload
+            )
         }
     }
 }
